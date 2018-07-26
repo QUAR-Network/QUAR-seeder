28c92cd11f85aa9e13bf59bae0dad48fc70670fa
QUAR-Network <41087566+QUAR-Network@users.noreply.github.com>
2018-07-26 16:18:26 +0100

    Update for QUAR

--- protocol.cpp
+++ protocol.cpp
@@ -22,7 +22,7 @@ static const char* ppszTypeName[] =
     "block",
 };
 
-unsigned char pchMessageStart[4] = { 0xf9, 0xbe, 0xb4, 0xd9 };
+unsigned char pchMessageStart[4] = { 0x6b, 0xb2, 0x9f, 0xd2 };
 
 CMessageHeader::CMessageHeader()
 {
